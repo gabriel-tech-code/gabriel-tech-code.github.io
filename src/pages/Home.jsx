356e7802dee04fd430946ce340b962005598aca9
Gabriel-Tech <gabriel.p.garcia.tech@proton.me>
2025-12-16 10:07:41 -0600

    added nav

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,22 +3,28 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div className="home-container">
+    <div className="home">
       <h1>Welcome to Gabriel’s Dev Hub</h1>
 
-      <div className="link-section">
+      <nav>
         <h2>Tailwind Pages</h2>
-        <Link to="/portfolio">Portfolio</Link>
+        
+        <Link to="/portfolio">Portfolio </Link >
+        
         <Link to="/resume">Resume</Link>
+        
         <Link to="/games">JavaScript Games</Link>
-      </div>
-
-      <div className="link-section">
+        
         <h2>Bootstrap Pages</h2>
+        
         <Link to="/work-projects">Work Projects</Link>
+        
         <Link to="/my-projects">My Projects</Link>
+        
         <Link to="/notes">Notes</Link>
-      </div>
+        
+      </nav>
+
     </div>
   );
 }
