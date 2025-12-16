d2cc7ee812fcce1c81dda913837cd69216060b04
Gabriel-Tech <gabriel.p.garcia.tech@proton.me>
2025-12-16 10:36:49 -0600

    Fix GitHub Actions depoyments

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
   base: "/gabriel-tech-code.github.io/",
-})
+  plugins: [tailwindcss(), react()],
+});
+
