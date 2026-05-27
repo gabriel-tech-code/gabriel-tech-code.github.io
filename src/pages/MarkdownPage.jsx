8589b433ed5f49c89dead477b0a5e3d3d0b668d6
gabriel-tech-code <gabriel.p.garcia.tech@proton.me>
2026-05-27 10:06:20 -0500

    removed import

--- src/pages/MarkdownPage.jsx
+++ src/pages/MarkdownPage.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import {marked} from 'marked';
+
 
 import Header from '../components/Header'
 import Footer from '../components/Footer'
