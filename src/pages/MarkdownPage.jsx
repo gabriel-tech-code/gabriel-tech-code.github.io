85c22d66076047020ff9b6212484609791914506
gabriel-tech-code <gabriel.p.garcia.tech@proton.me>
2026-05-27 09:33:46 -0500

    mod the markdown page to look like home page.

--- src/pages/MarkdownPage.jsx
+++ src/pages/MarkdownPage.jsx
@@ -2,11 +2,18 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import BodyGrid from '../components/BodyGrid'
+import CyanLogo from '../assets/images/Cyan_Logo.png'
+import TopicBlock from '../components/TopicBlock'
+
 export default function MarkdownPage() {
+  
   const { folder, file } = useParams();
   const [content, setContent] = useState("");
 
-  const url = `https://raw.githubusercontent.com/gabriel-tech-code/content/${folder}/${file}.md`;
+  const url = `https://raw.githubusercontent.com/gabriel-tech-code/content/main/${folder}/${file}.md`;
 
   useEffect(() => {
     fetch(url)
@@ -14,10 +21,18 @@ export default function MarkdownPage() {
       .then(text => setContent(text))
       .catch(() => setContent("# Error\nCould not load content."));
   }, [url]);
-
+  
   return (
-    <main className="max-w-4xl px-6 py-10 mx-auto prose dark:prose-invert">
-      <ReactMarkdown>{content}</ReactMarkdown>
-    </main>
-  );
+      <div className="flex flex-col min-h-screen bg-zinc-100 dark:bg-zinc-950">
+
+        <Header title="Gabriel Tech" subtitle="Frontend Developer · React · JavaScript"/>
+
+        <main className="flex-grow px-6 py-10 space-y-10">
+
+        <ReactMarkdown>{content}</ReactMarkdown>
+
+        </main>
+        <Footer />
+      </div>
+    )
 }
